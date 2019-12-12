5cecc17b48071ece683c87d05944c9db8ca319c0
patrick-chu <36650082+patrick-chu@users.noreply.github.com>
2019-12-12 14:34:15 -0800

    Initial commit

---
+++ src/main/java/com/ibm/inventory_management/services/StockItemService.java
@@ -0,0 +1,44 @@
+package com.ibm.inventory_management.services;
+
+import java.io.IOException;
+import java.util.List;
+import javax.annotation.PostConstruct;
+
+import com.cloudant.client.api.CloudantClient;
+import com.cloudant.client.api.Database;
+import org.springframework.stereotype.Service;
+
+import com.ibm.inventory_management.config.CloudantConfig;
+import com.ibm.inventory_management.models.StockItem;
+
+@Service
+public class StockItemService implements StockItemApi {
+    private CloudantConfig config;
+    private CloudantClient client;
+    private Database db = null;
+
+    public StockItemService(CloudantConfig config, CloudantClient client) {
+        this.config = config;
+        this.client = client;
+    }
+
+    @PostConstruct
+    public void init() {
+        db = client.database(config.getDatabaseName(), true);
+    }
+
+    @Override
+    public List<StockItem> listStockItems() throws Exception {
+
+        try {
+            return db.getAllDocsRequestBuilder()
+                    .includeDocs(true)
+                    .build()
+                    .getResponse()
+                    .getDocsAs(StockItem.class);
+
+        } catch (IOException e) {
+            throw new Exception("", e);
+        }
+    }
+}
